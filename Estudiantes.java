290ead162b3007098e72ba48f5b76d8c228aea87
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-02-04 17:52:33 -0600

    estudiantes

--- Estudiantes.java
+++ Estudiantes.java
@@ -1,6 +1,32 @@
-
 package LabTc3;
 
-public class Estudiantes {
+
+
+public class Estudiantes extends Personas{
+    
+
+    private int idEstudiante;
+
+    public Estudiantes(String nombre, int nacimiento,int idEstudiante) {
+        super(nombre, nacimiento);
+        this.idEstudiante = idEstudiante;
+    }
+
+
+    public int getIdEstudiantes() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiantes(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "nombre:" + nombre+", Nacimiento"+nacimiento+", ID de estudiante:"+idEstudiante;
+    }
+
+
     
 }
