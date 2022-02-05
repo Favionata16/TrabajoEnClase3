e72c9c2dc0a1361536960614bcecfcacb5cef65f
Ajborjas <98615109+Ajborjas@users.noreply.github.com>
2022-02-04 18:01:33 -0600

    se realizaron cambios

--- Transporte.java
+++ Transporte.java
@@ -2,5 +2,32 @@
 package LabTc3;
 
 public class Transporte {
+    protected int placa;
+    protected String color;
+
+    public Transporte(int placa, String color) {
+        this.placa = placa;
+        this.color = color;
+    }
+
+    public int getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(int placa) {
+        this.placa = placa;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    
+    
+    
     
 }
