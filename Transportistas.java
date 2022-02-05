f6877dbcd0b70166572c6c55543234454f9b5681
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-02-04 18:02:03 -0600

    se realizaron cambios

--- Transportistas.java
+++ Transportistas.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package LabTc3;
 
-/**
- *
- * @author daniel
- */
-public class Transportistas {
+public class Transportistas extends Personas{
+    
+    
+    private int experiencia;
+    private String apodo;
+
+    public Transportistas(String nombre, int nacimiento, int experiencia, String apodo) {
+        super(nombre, nacimiento);
+        this.experiencia = experiencia;
+        this.apodo=apodo;
+        
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre:" + nombre+", Nacimiento"+nacimiento+", Experiencia:"+experiencia+", Apodo en el barrio:"+apodo;
+    }
     
 }
